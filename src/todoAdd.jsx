8a06eae003137475c7e136740591e9f092dadc2b
PraveenRamamoorthi <139839468+PraveenRamamoorthi@users.noreply.github.com>
2023-10-03 15:17:29 +0530

    Update todoAdd.jsx

--- src/todoAdd.jsx
+++ src/todoAdd.jsx
@@ -1,16 +1,10 @@
-import {Box, Button} from '@mui/material';
-import {useState} from 'react';
+import {Box} from '@mui/material';
 function ListName(props) {
-  const [addedList, setAddedList] = useState(props.list);
-
-
   console.log(props.index);
   return (
     <Box className="content" style={{display: 'flex'}} key={props.index}>
-      {/* <p>{props.list}</p> */}
-      <p> {addedList}</p>
+      <p>{props.list}</p>
     </Box>
   );
 }
 export default ListName;
-
